404f65c6f0116193f38783bf19ea5707857bc513
Racopas <raphaelfutdaniel@gmail.com>
2024-10-13 17:52:29 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+console.log(listaSelecaoPokedevs);
+
+listaSelecaoPokedevs.forEach(pokedev =>{
+    pokedev.addEventListener('click', () => {
+        esconderCartaoPokedev();
+
+        const idPokedevSelecionado = mostrarCartaoSelecionado(pokedev);
+
+        desmarcarPokedevNaListagem();
+
+        marcarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+    })
+})
+
+function marcarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desmarcarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoSelecionado(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+    cartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
